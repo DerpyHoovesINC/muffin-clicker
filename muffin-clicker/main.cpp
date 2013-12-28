3460c4ff70ae45e161738d342f5366fffd9dd9c6
WebFreak001 <jan.jurzitza@gmail.com>
2013-12-28 01:34:51 +0100

    Added Background + Muffin

--- muffin-clicker/main.cpp
+++ muffin-clicker/main.cpp
@@ -4,6 +4,28 @@ int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 480), "Muffin clicker");
 
+	sf::Texture muffin;
+	if (!muffin.loadFromFile("../Debug/muffin.png"))
+	{
+		//Error handling
+	}
+	sf::Texture background;
+	if (!background.loadFromFile("../Debug/background.png"))
+	{
+		//Error handling
+	}
+	background.setRepeated(true);
+
+	sf::RectangleShape muffinSprite;
+	muffinSprite.setPosition(sf::Vector2f(10, 10));
+	muffinSprite.setTexture(&muffin);
+	muffinSprite.setSize(sf::Vector2f(256, 256));
+
+	sf::RectangleShape backgroundSprite(sf::Vector2f(800, 480));
+	backgroundSprite.setTexture(&background);
+	backgroundSprite.setPosition(sf::Vector2f(0, 0));
+	backgroundSprite.setTextureRect(sf::IntRect(0, 0, 800, 480));
+
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -14,8 +36,9 @@ int main()
 		}
 
 		window.clear();
+		window.draw(backgroundSprite);
 
-
+		window.draw(muffinSprite);
 
 		window.display();
 	}
